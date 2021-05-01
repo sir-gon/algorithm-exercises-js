e7421d1f517cc7a394f18ca8d69449c0aa97f3ed
Gonzalo Diaz <gdiazc@ripley.com>
2021-05-01 14:23:40 -0400

    [TEST] New test for problem0001

--- problem0001.js
+++ problem0001.js
@@ -1,20 +1,20 @@
 /**
- * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. * The sum of these multiples is 23.
- * Find the sum of all the multiples of 3 or 5 below 1000.
+ * https://projecteuler.net/problem=1
+ *
+ * Multiples of 3 and 5
  */
 
- function problem0001(_DEBUG = true) {
+function problem0001(_top, _DEBUG = true) {
   let total = 0;
-  const top = 1000;
 
-  for (let i = 0; i < top; i++) {
+  for (let i = 0; i < _top; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
-      if(_DEBUG) console.log(`i = ${i}`);
+      if (_DEBUG) console.log(`i = ${i}`);
       total += i;
     }
   }
 
-  if(_DEBUG) console.log(`TOTAL = ${total}`);
+  if (_DEBUG) console.log(`TOTAL = ${total}`);
 
   return total;
 }
--- problem0001.test.js
+++ problem0001.test.js
@@ -1,3 +1,12 @@
+/**
+ * Multiples of 3 and 5
+ *
+ * https://projecteuler.net/problem=1
+ *
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. * The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
 import { problem0001 } from './problem0001.js';
 
 const DEBUG =
@@ -8,7 +17,9 @@ describe('problem 0001', () => {
     expect.assertions(1);
 
     const solutionFound = 233168;
-    const calculated = problem0001(DEBUG);
+    const top = 1000;
+
+    const calculated = problem0001(top, DEBUG);
 
     console.log(calculated);
 
