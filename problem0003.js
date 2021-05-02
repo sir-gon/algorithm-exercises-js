9ebe5911922937d6cdc801465ccc0988b3a535cb
Gonzalo Diaz <gdiazc@ripley.com>
2021-05-02 13:22:53 -0400

    Manual prettier fix in problem0003.

--- problem0003.js
+++ problem0003.js
@@ -8,33 +8,30 @@ import { divisors, isPrime } from './helpers/index.js';
 
 function problem0003(_top, _DEBUG) {
   let middle;
-  let divs = divisors(_top, _DEBUG);
+  const divs = divisors(_top, _DEBUG);
   let i;
 
-  if (divs.length % 2 === 0)
-  {
-    middle = (divs.length/2) -1;
+  if (divs.length % 2 === 0) {
+    middle = divs.length / 2 - 1;
   } else {
-    middle = (divs.length) - 1;
+    middle = divs.length - 1;
   }
 
   // check half divisors, each is Prime? wich is largest?
   let maxPrimeFactor;
 
   i = middle;
-  do
-  {
-    let prime = isPrime(divs[i]);
+  do {
+    const prime = isPrime(divs[i]);
 
-    if (_DEBUG) console.log(`${divs[i]} is Prime? ${prime}`)
+    if (_DEBUG) console.log(`${divs[i]} is Prime? ${prime}`);
 
-    if(prime)
-    maxPrimeFactor = divs[i];
+    if (prime) maxPrimeFactor = divs[i];
 
-    i--;
-  } while(i >= 0 && !maxPrimeFactor)
+    i -= 1;
+  } while (i >= 0 && !maxPrimeFactor);
 
-  if (_DEBUG) console.log(`${maxPrimeFactor}`) // Solution found 6857
+  if (_DEBUG) console.log(`${maxPrimeFactor}`); // Solution found 6857
 
   return maxPrimeFactor;
 }
