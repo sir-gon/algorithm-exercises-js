dbda5dc738a180e2f5c1b3b8f1e0d58aaaa73663
Gonzalo Diaz <gdiazc@ripley.com>
2021-05-01 21:51:49 -0400

    New fiboStep() helper to produce the fibonacci series

--- helpers/index.js
+++ helpers/index.js
@@ -1,13 +1,8 @@
-import { isPalindrome } from './isPalindrome.js'
-import { isPrime } from './isPrime.js'
-import { divisors } from './divisors.js'
-import { product } from './product.js'
-import { sum } from './sum.js'
+import { fiboStep } from './fibonacci.js';
+import { isPalindrome } from './isPalindrome.js';
+import { isPrime } from './isPrime.js';
+import { divisors } from './divisors.js';
+import { product } from './product.js';
+import { sum } from './sum.js';
 
-export { 
-    isPalindrome,
-    isPrime,
-    divisors,
-    product,
-    sum
-}
+export { fiboStep, isPalindrome, isPrime, divisors, product, sum };
