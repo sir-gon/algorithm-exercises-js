32cfff0b96d491ad652f17f75534d74f193e0d5a
Gonzalo Diaz <gdiazc@ripley.com>
2022-05-20 13:14:59 -0400

    Problem 0023 partial.

--- src/problem0023.js
+++ src/problem0023.js
@@ -8,23 +8,36 @@
  *
  * ////////////////////////////////////////////////////////////////////////////
  *
- * Result found:
+ * Result sumsAbundantNums:
  * ////////////////////////////////////////////////////////////////////////////
  */
 
+import util from 'util';
 import {
   abundance,
   ___DIVISORS_DEFICIENT___,
-  ___DIVISORS_PERFECT___
+  ___DIVISORS_PERFECT___,
+  ___DIVISORS_ABUNDANT___
 } from './helpers/index.js';
+
+util.inspect.defaultOptions.maxArrayLength = null;
+util.inspect.defaultOptions.colors = true;
+util.inspect.defaultOptions.depth = null;
+util.inspect.defaultOptions.showHidden = false;
 
 export function problem0023(_DEBUG) {
   const result = null;
 
   const underLimit = 12;
   const superLimit = 28123;
-  const nonAbundantList = {};
+  const allNumsList = [];
+  const nonAbundantList = [];
 
+  for (let i = 1; i < underLimit; i++) {
+    allNumsList.push(i);
+  }
+
+  // Produce a list of abundant numbers below limit
   for (let i = underLimit; i <= superLimit; i++) {
     const abundancyOf = abundance(i);
 
@@ -32,13 +45,53 @@ export function problem0023(_DEBUG) {
       abundancyOf !== ___DIVISORS_DEFICIENT___ &&
       abundancyOf !== ___DIVISORS_PERFECT___
     ) {
-      nonAbundantList[i] = i;
+      nonAbundantList.push(i);
+    }
+
+    allNumsList.push(i);
+  }
+
+  console.log(
+    'all nums list size:',
+    allNumsList.length,
+    `=>`,
+    util.inspect(allNumsList, {})
+  );
+
+  console.log(
+    'non abundant nums list:',
+    nonAbundantList.length,
+    `=>`,
+    util.inspect(nonAbundantList, {})
+  );
+
+  let sumsAbundantNums = [];
+
+  // Produce a list of sums of pair of abundant numbers below limit
+  for (let i = 0; i < nonAbundantList.length; i++) {
+    // console.log(`${i} => ${nonAbundantList[i]}`);
+
+    for (
+      let j = 0;
+      nonAbundantList[i] + nonAbundantList[j] <= superLimit &&
+      j < nonAbundantList.length;
+      j++
+    ) {
+      sumsAbundantNums.push(nonAbundantList[i] + nonAbundantList[j]);
     }
   }
 
-  // console.log(divisors(16));
+  // filter duplicates
+  sumsAbundantNums = [...new Set(sumsAbundantNums)];
 
-  console.log(`result`, nonAbundantList);
+  console.log(
+    'sumsAbundantNums size:',
+    sumsAbundantNums.length,
+    `result`,
+    util.inspect(sumsAbundantNums, {})
+  );
+
+
 
   if (_DEBUG) {
     console.log(`result`, result);
