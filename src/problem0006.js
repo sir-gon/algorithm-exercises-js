432efc8b585e8348e8a65ae13ccca69d5fa7dfe1
Gonzalo Diaz <gdiazc@ripley.com>
2022-06-17 11:20:11 -0400

    Problem0006: better comments

--- src/problem0006.js
+++ src/problem0006.js
@@ -1,12 +1,31 @@
-// problem0006.js
-
-// The sum of the squares of the first ten natural numbers is,
-// [image unavailable]
-// The square of the sum of the first ten natural numbers is,
-// [image unavailable]
-// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
-// [image unavailable]
-// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+/**
+ * Sum square difference
+ *
+ * https://projecteuler.net/problem=6
+ *
+ * The sum of the squares of the first ten natural numbers is,
+ *
+ * 1² * 2² * ... * 10² = 385
+ *
+ * The square of the sum of the first ten natural numbers is,
+ *
+ * (1 * 2 * ... * 10)² = 3025
+ *
+ * Hence the difference between the sum of the squares of the
+ * first ten natural numbers and the square of the sum is
+ * 3025 - 385.
+ *
+ * Find the difference between the sum of the squares of the
+ * first one hundred natural numbers and the square of the sum.
+ *
+ * ////////////////////////////////////////////////////////////////////////////
+ * Solution found:
+ * Sum of first 100 squares = 338350
+ * Base for Square Of Sum of first 100 = 5050
+ * Square Of Sum of first 100 = 25502500
+ * Difference found 25502500 - 338350 =  25164150
+ * ////////////////////////////////////////////////////////////////////////////
+ */
 
 import logger from './logger.js';
 
--- src/problem0006.test.js
+++ src/problem0006.test.js
@@ -3,6 +3,21 @@
  *
  * https://projecteuler.net/problem=6
  *
+ * The sum of the squares of the first ten natural numbers is,
+ *
+ * 1² * 2² * ... * 10² = 385
+ *
+ * The square of the sum of the first ten natural numbers is,
+ *
+ * (1 * 2 * ... * 10)² = 3025
+ *
+ * Hence the difference between the sum of the squares of the
+ * first ten natural numbers and the square of the sum is
+ * 3025 - 385.
+ *
+ * Find the difference between the sum of the squares of the
+ * first one hundred natural numbers and the square of the sum.
+ *
  * ////////////////////////////////////////////////////////////////////////////
  * Solution found:
  * Sum of first 100 squares = 338350
