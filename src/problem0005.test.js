5bb904ba2522203bfd967f2b554166d89bedab33
Gonzalo Diaz <gdiazc@ripley.com>
2022-06-27 14:21:49 -0400

    Problem0005: Better comments

--- src/problem0005.test.js
+++ src/problem0005.test.js
@@ -1,4 +1,3 @@
-/* eslint-disable jest/no-disabled-tests */
 /**
  * Smallest multiple
  *
@@ -8,21 +7,21 @@
  *  2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
  *
  *  What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
- *
- * ////////////////////////////////////////////////////////////////////////////
- * NOTE: I think a better way to solve problem 0005.
- * If I got prime numbers in beetwen [1,20], can I test them as
- * prime factos, testing a combinatorial exponents that satisfies
- * the condition that the number found is divisible by all
- * the numbers in beetwen [1,20],
- * Instead, this solution is brute force.
- *
- / ////////////////////////////////////////////////////////////////////////////
- * Solution found by BRUTE FORCE running for a LONG TIME this routine
- *
- * FOUND: 232792560 divisible by any element beetwen 1 and 20
- / ////////////////////////////////////////////////////////////////////////////
  */
+
+/// ////////////////////////////////////////////////////////////////////////////
+// NOTE: I think a better way to solve problem 0005.
+// If I got prime numbers in beetwen [1,20], can I test them as
+// prime factors, testing a combinatorial exponents that satisfies
+// the condition that the number found is divisible by all
+// the numbers in beetwen [1,20],
+// Instead, this solution is brute force.
+
+/// ////////////////////////////////////////////////////////////////////////////
+// Solution found after a LONG TIME running this routine:
+//
+// FOUND: 232792560 divisible by any element beetwen 1 and 20
+/// ////////////////////////////////////////////////////////////////////////////
 
 import logger from './logger.js';
 
