92d402e55b91435841dd55bcc145b28840b378c0
Gonzalo Diaz <gdiazc@ripley.com>
2022-06-27 13:51:03 -0400

    Problem0003: Better comments

--- src/problem0003.js
+++ src/problem0003.js
@@ -1,8 +1,12 @@
-// problem0003.js
-
-// The prime factors of 13195 are 5, 7, 13 and 29.
-
-// What is the largest prime factor of the number 600851475143 ?
+/**
+ * Largest prime factor
+ *
+ * https://projecteuler.net/problem=3
+ *
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the number 600851475143 ?
+ */
 
 import logger from './logger.js';
 
