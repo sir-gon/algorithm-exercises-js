ff040e37b1e13e40c38c6c6f50f23842cb15363e
Gonzalo Diaz <gdiazc@ripley.com>
2022-06-27 14:27:50 -0400

    Problem0007: Better comments

--- src/problem0007.js
+++ src/problem0007.js
@@ -1,6 +1,14 @@
-// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-
-// What is the 10 001st prime number?
+/**
+ * 10001st prime
+ *
+ * https://projecteuler.net/problem=7
+ *
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+ * we can see that the 6th prime is 13.
+ *
+ * What is the 10 001st prime number?
+ *
+ */
 
 import logger from './logger.js';
 import { isPrime } from './helpers/index.js';
