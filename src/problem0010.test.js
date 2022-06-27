fb3b9e3cbb5fd5372f525d3364ae777285546e94
Gonzalo Diaz <gdiazc@ripley.com>
2022-06-27 14:37:55 -0400

    Problem0010: Better comments

--- src/problem0010.test.js
+++ src/problem0010.test.js
@@ -1,25 +1,26 @@
 /* eslint-disable jest/no-disabled-tests */
+
 /**
+ * Summation of primes
  *
+ * https://projecteuler.net/problem=10
  *
  * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
  *
  * Find the sum of all the primes below two million.
- *
- * ////////////////////////////////////////////////////////////////////////////
- *
- * About solution: BRUTE FORCE
- *
- * Found:
- * ...
- * Prime found 1999969 put in position: 148931
- * Prime found 1999979 put in position: 148932
- * Prime found 1999993 put in position: 148933
- * Sum of primes below 2000000 is: 142913828922
- *
- * ////////////////////////////////////////////////////////////////////////////
- *
  */
+
+// ////////////////////////////////////////////////////////////////////////////
+// About solution: BRUTE FORCE
+//
+// Found:
+// ...
+// Prime found 1999969 put in position: 148931
+// Prime found 1999979 put in position: 148932
+// Prime found 1999993 put in position: 148933
+// Sum of primes below 2000000 is: 142913828922
+//
+// ////////////////////////////////////////////////////////////////////////////
 
 import logger from './logger.js';
 
