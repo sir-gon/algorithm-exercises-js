bd05aadbc0d60d0de49d4a089d4779f9a8463dbd
Gonzalo Diaz <gdiazc@ripley.com>
2022-06-27 14:35:39 -0400

    Problem0009: Better comments

--- src/problem0009.test.js
+++ src/problem0009.test.js
@@ -4,19 +4,19 @@
  * https://projecteuler.net/problem=9
  *
  * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-
+ *
  * a² + b² = c²
  * For example, 3² + 4² = 9 + 16 = 25 = 5².
-
+ *
  * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
  * Find the product abc.
- *
- * ////////////////////////////////////////////////////////////////////////////
- * Solution found:
- * FOUND: a = 200 b = 375 c = 425
- * PRODUCT: a * b * c = 31875000
- * ////////////////////////////////////////////////////////////////////////////
  */
+
+// ////////////////////////////////////////////////////////////////////////////
+// Solution found:
+// FOUND: a = 200 b = 375 c = 425
+// PRODUCT: a * b * c = 31875000
+// ////////////////////////////////////////////////////////////////////////////
 
 import logger from './logger.js';
 
