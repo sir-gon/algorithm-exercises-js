c8788bc0301093965a60e5446b6a78fafa3f49db
Gonzalo Diaz <gdiazc@ripley.com>
2022-06-27 13:59:12 -0400

    Problem0004: Better comments

--- src/problem0004.test.js
+++ src/problem0004.test.js
@@ -3,7 +3,11 @@
  *
  * https://projecteuler.net/problem=4
  *
- * Solution found: Largest Palindrome => 993 𝗑 913 = 906609
+ * A palindromic number reads the same both ways.
+ * The largest palindrome made from the product of two 2-digit
+ * numbers is 9009 = 91 × 99.
+ *
+ * Find the largest palindrome made from the product of two 3-digit numbers.
  */
 
 import logger from './logger.js';
