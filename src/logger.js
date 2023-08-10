7ceab52fdb9e73e03a947da5247ce3baa4549e88
Gonzalo Diaz <devel@gon.cl>
2023-08-09 20:18:17 -0400

    [FIX] This should be done within a hook / ESLint

--- src/logger.js
+++ src/logger.js
@@ -4,48 +4,29 @@
 
 import pino from 'pino';
 
-const Singleton = (() => {
-  let instance;
-
-  function createInstance() {
-    const logger = pino({
-      customLevels: {
-        log: 30
-      },
-      transport: {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          // sync: true,
-          ignore: 'pid,hostname'
-        }
-      },
-      level: process.env.LOG_LEVEL?.toLowerCase() || 'fatal'
-    });
-
-    console.log(`LOG: Instantiated at: ${new Date().toLocaleString()}`);
-    logger.debug('LOG: DEBUG LEVEL ENABLED');
-    logger.error('LOG: ERROR LEVEL ENABLED');
-    logger.warn('LOG: WARN ENABLED');
-    logger.log('LOG: INFO LEVEL ENABLED');
-
-    logger.log(`LOG LEVEL: ${logger.levelVal}`);
-
-    return logger;
-  }
-
-  return {
-    getInstance() {
-      if (!instance) {
-        instance = createInstance();
-      }
-
-      return instance;
+const logger = pino({
+  customLevels: {
+    log: 30
+  },
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      // sync: true,
+      ignore: 'pid,hostname'
     }
-  };
-})();
+  },
+  level: process.env.LOG_LEVEL?.toLowerCase() ?? 'fatal'
+});
 
-const logger = Singleton.getInstance();
+(() => {
+  console.log(`LOG: Instantiated at: ${new Date().toLocaleString()}`);
+  console.log(`LOG LEVEL: ${logger.levelVal}`);
+  logger.debug('LOG: DEBUG LEVEL ENABLED');
+  logger.error('LOG: ERROR LEVEL ENABLED');
+  logger.warn('LOG: WARN ENABLED');
+  logger.info('LOG: INFO LEVEL ENABLED');
+})();
 
 export default logger;
 export { logger };
