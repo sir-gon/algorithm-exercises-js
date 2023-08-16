5c0e6d403cc6a7ea0fdc8e0b6ce5cb04eb6ef4c5
Gonzalo Diaz <devel@gon.cl>
2023-08-16 01:07:05 -0400

    [Hacker Rank]: Sales by Match solved ✓

---
+++ src/hackerrank/implementation/sockMerchant.js
@@ -0,0 +1,27 @@
+/**
+ * @link Problem definition [[docs/hackerrank/implementation/sockMerchant.md]]
+ */
+
+import { logger as console } from '../../logger.js';
+
+export function sockMerchant(n, ar) {
+  let result = 0;
+
+  const matches = {};
+
+  ar.forEach((v) => {
+    matches[v] = matches?.[v] ? matches[v] + 1 : 1;
+  });
+
+  console.debug(matches);
+
+  for (const [k, value] of Object.entries(matches)) {
+    console.debug(`k: ${k}, value: ${value}`);
+
+    result += Math.floor(value / 2);
+  }
+
+  return result;
+}
+
+export default { sockMerchant };
