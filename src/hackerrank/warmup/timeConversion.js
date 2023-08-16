71ad9ba9f301b2cade44680563dc3f770d79ce25
Gonzalo Diaz <devel@gon.cl>
2023-08-15 23:41:20 -0400

    [Hacker Rank]: timeConversion solved ✓

---
+++ src/hackerrank/warmup/timeConversion.js
@@ -0,0 +1,28 @@
+/**
+ * @link Problem definition [[docs/hackerrank/warmup/timeConversion.md]]
+ */
+
+export function timeConversion(s) {
+  let meridian = s.substring(s.length - 2, s.length);
+  meridian = meridian.toLowerCase();
+
+  const timeStr = s.substring(0, s.length - 2);
+  const time = timeStr.split(':');
+  let hour = parseInt(time[0], 10);
+
+  if (hour >= 12) {
+    hour = 0;
+  }
+
+  if (meridian === 'pm') {
+    hour += 12;
+  }
+
+  time[0] = hour.toString().padStart(2, '0');
+
+  const conversion = time.join(':');
+
+  return conversion;
+}
+
+export default { timeConversion };
---
+++ src/hackerrank/warmup/timeConversion.test.js
@@ -0,0 +1,32 @@
+import { logger as console } from '../../logger.js';
+import { timeConversion } from './timeConversion.js';
+
+describe('timeConversion', () => {
+  it('timeConversion example', () => {
+    expect.assertions(2);
+
+    const tests = [
+      { input: '12:01:00PM', answer: '12:01:00' },
+      { input: '12:01:00AM', answer: '00:01:00' }
+    ];
+
+    for (const test of tests) {
+      const calculated = timeConversion(test.input);
+      console.log(`timeConversion(${test.input}) Test case 0: ${calculated}`);
+      expect(calculated).toBe(test.answer);
+    }
+  });
+
+  it('timeConversion test case 0', () => {
+    expect.assertions(1);
+
+    const input = '07:05:45PM';
+    const solutionFound = '19:05:45';
+
+    const calculated = timeConversion(input);
+
+    console.log(`timeConversion solution found: ${calculated}`);
+
+    expect(calculated).toBe(solutionFound);
+  });
+});
