f7800ef3d67d45b0b0d37a88918860195e17e614
Gonzalo Diaz <devel@gon.cl>
2023-08-15 23:58:20 -0400

    [Hacker Rank]: gradingStudents solved ✓

---
+++ src/hackerrank/implementation/gradingStudents.test.js
@@ -0,0 +1,17 @@
+import { logger as console } from '../../logger.js';
+import { gradingStudents } from './gradingStudents.js';
+
+describe('gradingStudents', () => {
+  it('gradingStudents Test case 0', () => {
+    expect.assertions(1);
+
+    const input = [73, 67, 38, 33];
+    const solutionFound = [75, 67, 40, 33];
+
+    const calculated = gradingStudents(input);
+
+    console.log(`gradingStudents Test case 0: ${calculated}`);
+
+    expect(calculated).toStrictEqual(solutionFound);
+  });
+});
