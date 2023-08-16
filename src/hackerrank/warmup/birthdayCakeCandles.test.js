dba46e482f84c0fe466eb179fd254746f2e71b0a
Gonzalo Diaz <devel@gon.cl>
2023-08-15 23:34:45 -0400

    [Hacker Rank]: birthdayCakeCandles solved ✓

---
+++ src/hackerrank/warmup/birthdayCakeCandles.test.js
@@ -0,0 +1,29 @@
+import { logger as console } from '../../logger.js';
+import { birthdayCakeCandles } from './birthdayCakeCandles.js';
+
+describe('birthdayCakeCandles', () => {
+  it('miniMaxSum border case', () => {
+    expect.assertions(1);
+
+    expect(() => {
+      birthdayCakeCandles([]);
+    }).toThrow('Empty input');
+  });
+
+  it('birthdayCakeCandles Test case 0', () => {
+    expect.assertions(2);
+
+    const tests = [
+      { input: [3, 2, 1, 3], answer: 2 },
+      { input: [1, 2, 3, 3], answer: 2 }
+    ];
+
+    for (const test of tests) {
+      const calculated = birthdayCakeCandles(test.input);
+      console.log(
+        `birthdayCakeCandles(${test.input}) Test case 0: ${calculated}`
+      );
+      expect(calculated).toBe(test.answer);
+    }
+  });
+});
