8fa4b3710cedaf6d124ff696de96c515066109fc
Gonzalo Diaz <devel@gon.cl>
2023-08-16 00:02:02 -0400

    [Hacker Rank]: countApplesAndOranges solved ✓

---
+++ src/hackerrank/implementation/countApplesAndOranges.test.js
@@ -0,0 +1,22 @@
+import { logger as console } from '../../logger.js';
+import { countApplesAndOranges } from './countApplesAndOranges.js';
+
+describe('countApplesAndOranges', () => {
+  it('countApplesAndOranges test case 0', () => {
+    expect.assertions(1);
+
+    const s = 7;
+    const t = 11;
+    const a = 5;
+    const b = 15;
+    const apples = [-2, 2, 1];
+    const oranges = [5, -6];
+    const solutionFound = [1, 1].join('\n');
+
+    const calculated = countApplesAndOranges(s, t, a, b, apples, oranges);
+
+    console.log(`countApplesAndOranges test case 0: ${calculated}`);
+
+    expect(calculated).toBe(solutionFound);
+  });
+});
