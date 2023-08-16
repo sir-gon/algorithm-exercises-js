b2487046519eed95294832b9971593a859bc7cf9
Gonzalo Diaz <devel@gon.cl>
2023-08-16 00:25:38 -0400

    [Hacker Rank]: Divisible Sum Pairs solved ✓

---
+++ src/hackerrank/implementation/divisibleSumPairs.js
@@ -0,0 +1,21 @@
+/**
+ * @link Problem definition [[docs/hackerrank/implementation/divisibleSumPairs.md]]
+ */
+
+import { logger as console } from '../../logger.js';
+
+export function divisibleSumPairs(n, k, ar) {
+  let pairs = 0;
+  for (let i = 0; i < ar.length; i++) {
+    for (let j = i + 1; j < ar.length; j++) {
+      if ((ar[i] + ar[j]) % k === 0) {
+        console.debug(`i: ${i} => ${ar[i]} | j: ${j} => ${ar[j]}`);
+        pairs += 1;
+      }
+    }
+  }
+
+  return pairs;
+}
+
+export default { divisibleSumPairs };
