203c6b6f675c2780f1b532507a6fb7f461f63f74
Gonzalo Diaz <devel@gon.cl>
2023-08-15 23:04:35 -0400

    [Hacker Rank]: Diagonal Difference solved ✓

---
+++ src/hackerrank/warmup/diagonalDifference.js
@@ -0,0 +1,26 @@
+/**
+ * @link Problem definition [[docs/hackerrank/warmup/diagonalDifference.md]]
+ */
+
+import { logger as console } from '../../logger.js';
+
+export function diagonalDifference(arr) {
+  let diag1 = 0;
+  let diag2 = 0;
+  const last = arr.length - 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i === j) {
+        diag1 += arr[i][j];
+        diag2 += arr[last - i][j];
+      }
+    }
+  }
+  console.log(`diag1 ${String(diag1)}`);
+  console.log(`diag2 ${String(diag2)}`);
+
+  return Math.abs(diag1 - diag2);
+}
+
+export default { diagonalDifference };
