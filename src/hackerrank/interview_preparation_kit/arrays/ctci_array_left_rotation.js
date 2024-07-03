19d5f2ab955eced160d82b344519582d9dd384a1
Gonzalo Diaz <devel@gon.cl>
2024-07-03 17:30:41 -0400

    [Hacker Rank] Interview Preparation Kit: Arrays: Left Rotation. Solved ✅.

---
+++ src/hackerrank/interview_preparation_kit/arrays/ctci_array_left_rotation.js
@@ -0,0 +1,24 @@
+/**
+ * @link Problem definition [[docs/hackerrank/interview_preparation_kit/arrays/ctci_array_left_rotation.md]]
+ */
+
+export function rotLeftOne(aNumbers) {
+  const first = aNumbers.shift();
+  if (first !== undefined) {
+    aNumbers.push(first);
+  }
+
+  return aNumbers;
+}
+
+export function rotLeft(aNumbers, dRotations) {
+  let output = [...aNumbers];
+
+  for (let i = 0; i < dRotations; i++) {
+    output = rotLeftOne(output);
+  }
+
+  return output;
+}
+
+export default { rotLeft, rotLeftOne };
