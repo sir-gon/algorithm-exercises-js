a525def40d0c16d52b377e52ae612a5aba8cd468
Gonzalo Diaz <devel@gon.cl>
2024-07-19 01:05:06 -0400

    [Hacker Rank] Interview Preparation Kit: Dictionaries and Hashmaps: Count Triplets. Solved ✅.

---
+++ src/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/count_triplets_1_optmized.js
@@ -0,0 +1,38 @@
+/**
+ * @link Problem definition [[docs/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/count_triplets_1.md]]
+ * @see Solution Notes: [[docs/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/count_triplets_1-solution-notes.md]]
+ */
+
+export function countTriplets(arr, ratio) {
+  let triplets = 0;
+
+  const aCounter = arr.reduce((accumulator, entry) => {
+    if (entry in accumulator) {
+      accumulator[entry] += 1;
+    } else {
+      accumulator[entry] = 1;
+    }
+    return accumulator;
+  }, {});
+
+  const bCounter = {};
+
+  arr.forEach((x) => {
+    const j = Math.floor(x / ratio);
+    const k = x * ratio;
+    aCounter[x] -= 1;
+    if (bCounter[j] && aCounter[k] && x % ratio === 0) {
+      triplets += bCounter[j] * aCounter[k];
+    }
+
+    if (x in bCounter) {
+      bCounter[x] += 1;
+    } else {
+      bCounter[x] = 1;
+    }
+  });
+
+  return triplets;
+}
+
+export default { countTriplets };
