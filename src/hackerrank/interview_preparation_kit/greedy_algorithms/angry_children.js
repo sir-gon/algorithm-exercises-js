1cf7287cab8838d82fe99b4382850e6f95fa91ee
Gonzalo Diaz <devel@gon.cl>
2024-08-03 13:04:06 -0400

    [Hacker Rank] Interview Preparation Kit: Greedy Algorithms: Max Min. Solved ✅.

---
+++ src/hackerrank/interview_preparation_kit/greedy_algorithms/angry_children.js
@@ -0,0 +1,19 @@
+/**
+ * @link Problem definition [[docs/hackerrank/interview_preparation_kit/greedy_algorithms/angry-children.md]]
+ */
+
+export function maxMin(k, arr) {
+  const sortedlist = arr.map((x) => x).sort((a, b) => a - b);
+
+  let result = sortedlist[sortedlist.length - 1] - sortedlist[0];
+
+  for (let i = 0; i < sortedlist.length - k + 1; i++) {
+    const tmin = sortedlist[i];
+    const tmax = sortedlist[i + k - 1];
+    result = Math.min(result, tmax - tmin);
+  }
+
+  return result;
+}
+
+export default { maxMin };
