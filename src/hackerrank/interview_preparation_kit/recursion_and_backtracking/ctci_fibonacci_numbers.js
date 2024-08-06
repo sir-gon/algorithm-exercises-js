3728a64cc21f172e5a9a546fc31b0632e0a208f8
Gonzalo Diaz <devel@gon.cl>
2024-08-06 13:30:20 -0400

    [Hacker Rank] Interview Preparation Kit: Recursion: Fibonacci Numbers. Solved ✅.

---
+++ src/hackerrank/interview_preparation_kit/recursion_and_backtracking/ctci_fibonacci_numbers.js
@@ -0,0 +1,16 @@
+/**
+ * @link Problem definition [[docs/hackerrank/interview_preparation_kit/recursion_and_backtracking/ctci_fibonacci_numbers.md]]
+ */
+
+export function fibonacci(n) {
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+export default { fibonacci };
---
+++ src/hackerrank/interview_preparation_kit/recursion_and_backtracking/ctci_fibonacci_numbers.test.js
@@ -0,0 +1,19 @@
+import { describe, expect, it } from '@jest/globals';
+import { logger as console } from '../../../logger.js';
+
+import { fibonacci } from './ctci_fibonacci_numbers.js';
+import TEST_CASES from './ctci_fibonacci_numbers.testcases.json';
+
+describe('ctci_fibonacci_numbers', () => {
+  it('fibonacci test cases', () => {
+    expect.assertions(3);
+
+    TEST_CASES.forEach((test) => {
+      const answer = fibonacci(test.input);
+
+      console.debug(`fibonacci(${test.input}) solution found: ${answer}`);
+
+      expect(answer).toStrictEqual(test.expected);
+    });
+  });
+});
