391cd312acfb37572ae84d25b2cfc34bbbde67cc
Gonzalo Diaz <devel@gon.cl>
2024-08-09 16:54:24 -0400

    [Hacker Rank] Interview Preparation Kit: Recursion: Recursive Digit Sum. Solved ✅.

--- src/hackerrank/interview_preparation_kit/recursion_and_backtracking/recursive_digit_sum.js
+++ src/hackerrank/interview_preparation_kit/recursion_and_backtracking/recursive_digit_sum.js
@@ -1,1 +1,31 @@
-// [[docs/hackerrank/interview_preparation_kit/recursion_and_backtracking/recursive-digit-sum.md]]
+/**
+ * @link Problem definition [[docs/hackerrank/interview_preparation_kit/recursion_and_backtracking/recursive-digit-sum.md]]
+ */
+
+const RADIX = 10;
+
+export function superDigitCompute(n) {
+  if (n.length === 1) {
+    return parseInt(n, RADIX);
+  }
+
+  let partial = 0;
+  for (const digit of n) {
+    partial += parseInt(digit, RADIX);
+  }
+
+  return superDigitCompute(`${partial}`);
+}
+
+export function superDigit(n, k) {
+  const accumulator = `${superDigitCompute(n)}`;
+
+  let result = '';
+  for (let i = 0; i < k; i++) {
+    result = `${result}${accumulator}`;
+  }
+
+  return superDigitCompute(result);
+}
+
+export default { superDigit };
