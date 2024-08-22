b09f351b4e7543d54a8e46fdca480db0dc04066d
Gonzalo Diaz <devel@gon.cl>
2024-08-21 20:26:40 -0400

    [REFACTOR] [Hacker Rank] Interview Preparation Kit: Search: Swap Nodes [Algo]. New tests added.

--- src/hackerrank/interview_preparation_kit/search/swap_nodes_algo.test.js
+++ src/hackerrank/interview_preparation_kit/search/swap_nodes_algo.test.js
@@ -1,29 +1,35 @@
 import { describe, expect, it } from '@jest/globals';
 
-import { Tree, swapNodes } from './swap_nodes_algo.js';
+import { Node } from '../../lib/Node.js';
+import { Tree, swapNodes, __INITIAL_LEVEL__ } from './swap_nodes_algo.js';
 import TESTCASES from './swap_nodes_algo.testcases.json';
 
 describe('swap_nodes_algo', () => {
   it('testbuildTree_empty', () => {
-    expect.assertions(2);
+    expect.assertions(4);
 
-    const tInput = [];
-    const tree = new Tree(tInput);
+    const input = [];
+    const tree = new Tree(input);
     const expected = [1];
 
     expect(tree.flatTree()).toStrictEqual(expected);
     expect(tree.getRoot()).not.toBeNull();
+    expect(tree.getRoot()).toBeInstanceOf(Node);
+    expect(tree.getRoot().data).toBe(__INITIAL_LEVEL__);
   });
 
   it('testbuild_malformed_tree', () => {
-    expect.assertions(1);
+    expect.assertions(4);
 
     const input = [[], []];
     const tree = new Tree(input);
     const tresult = tree.flatTree();
-    const expected = [1];
+    const expected = [__INITIAL_LEVEL__];
 
     expect(tresult).toStrictEqual(expected);
+    expect(tree.getRoot()).not.toBeNull();
+    expect(tree.getRoot()).toBeInstanceOf(Node);
+    expect(tree.getRoot().data).toBe(__INITIAL_LEVEL__);
   });
 
   it('build tree and flattened tree test cases', () => {
