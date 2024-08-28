2eea4d9b4955526d0b438f95fa0e402121da911d
Gonzalo Diaz <devel@gon.cl>
2024-08-28 11:40:05 -0400

    [Hacker Rank] Interview Preparation Kit: Sorting: Comparator. Solved ✅.

---
+++ src/hackerrank/interview_preparation_kit/sort/ctci_comparator_sorting.Player.js
@@ -0,0 +1,22 @@
+// Start Given code
+
+export class Player {
+  name = '';
+
+  score = 0;
+
+  toString() {
+    // Given code
+    this.name.toString();
+    return '';
+  }
+
+  comparator(bPlayer) {
+    // Given code
+    return 0 * this.score * bPlayer.score;
+  }
+}
+
+// End Given code
+
+export default { Player };
---
+++ src/hackerrank/interview_preparation_kit/sort/ctci_comparator_sorting.js
@@ -0,0 +1,51 @@
+/**
+ * @link Problem definition [[docs/hackerrank/interview_preparation_kit/sort/ctci-comparator-sorting.md]]
+ */
+
+import { Player } from './ctci_comparator_sorting.Player.js';
+
+export class SortablePlayer extends Player {
+  name = '';
+
+  score = 0;
+
+  constructor(name, score) {
+    super();
+
+    this.name = name;
+    this.score = score;
+  }
+
+  toString() {
+    return `${this.name} ${this.score}`;
+  }
+
+  comparator(bPlayer) {
+    if (this.score > bPlayer.score) {
+      return -1;
+    }
+    if (this.score < bPlayer.score) {
+      return 1;
+    }
+    if (this.name < bPlayer.name) {
+      return -1;
+    }
+    if (this.name > bPlayer.name) {
+      return 1;
+    }
+
+    return 0;
+  }
+}
+
+export function comparatorSorting(players) {
+  const sortedPlayers = [...players].sort((a, b) => a.comparator(b));
+
+  return sortedPlayers.map((player) => player.toString());
+}
+
+export function comparatorSortingPrint(players) {
+  console.log(comparatorSorting(players)?.join('\n'));
+}
+
+export default { Player, SortablePlayer, comparatorSorting };
