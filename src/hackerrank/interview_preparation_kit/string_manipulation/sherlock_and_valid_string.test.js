3f4c4864fae37f4081aef644531156484ed10822
Gonzalo Diaz <devel@gon.cl>
2024-08-31 16:11:34 -0400

    [Hacker Rank] Interview Preparation Kit: String Manipulation: Sherlock and the Valid String. Solved ✅.

---
+++ src/hackerrank/interview_preparation_kit/string_manipulation/sherlock_and_valid_string.test.js
@@ -0,0 +1,18 @@
+import { describe, expect, it } from '@jest/globals';
+
+import { isValid } from './sherlock_and_valid_string.js';
+import TEST_CASES from './sherlock_and_valid_string.testcases.json';
+
+describe('isValid', () => {
+  it('isValid test cases', () => {
+    expect.assertions(9);
+
+    const __YES__ = 'YES';
+
+    TEST_CASES.forEach((test) => {
+      const result = isValid(test.input) === __YES__;
+
+      expect(result).toStrictEqual(test.expected);
+    });
+  });
+});
