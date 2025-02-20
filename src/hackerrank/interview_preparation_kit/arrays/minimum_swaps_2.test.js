b884c28977e60b190b2276702df54d5e57ac56eb
Gonzalo Diaz <devel@gon.cl>
2025-02-20 17:11:09 -0300

    [Hacker Rank] Interview Preparation Kit: Arrays: Minimum Swaps 2. Solved ✅. Adjusted the interface to match what hackerrank expects.

--- src/hackerrank/interview_preparation_kit/arrays/minimum_swaps_2.test.js
+++ src/hackerrank/interview_preparation_kit/arrays/minimum_swaps_2.test.js
@@ -1,20 +1,16 @@
 import { describe, expect, it } from '@jest/globals';
 import { logger as console } from '../../../logger.js';
 
-import { minimumSwaps } from './minimum_swaps_2.js';
+import ms2 from './minimum_swaps_2.js';
 
-const TEST_CASES = [
-  { title: 'Sample input 0', input: [4, 3, 1, 2], expected: 3 },
-  { title: 'Sample input 1', input: [2, 3, 4, 1, 5], expected: 3 },
-  { title: 'Sample input 2', input: [1, 3, 5, 2, 4, 6, 7], expected: 3 }
-];
+import TEST_CASES from './minimum_swaps_2.testcases.json';
 
 describe('minimum swaps 2', () => {
   it('minimumSwaps', () => {
     expect.assertions(3);
 
     TEST_CASES.forEach((test) => {
-      const answer = minimumSwaps(test.input);
+      const answer = ms2.minimumSwaps(test.input);
 
       console.debug(`minimumSwaps(${test.input}) solution found: ${answer}`);
 
