c46c3d1179de0549c127fa1f042edecd5af66bd5
Gonzalo Diaz <gonzalo@gon.cl>
2025-03-05 15:38:43 -0300

    [REFACTOR] [Hacker Rank] Interview Preparation Kit: Dictionaries and Hashmaps: Two Strings. * TEST data moved to JSON. * Adjusted the interface to match what hackerrank expects.

--- src/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/two_strings.test.js
+++ src/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/two_strings.test.js
@@ -2,71 +2,14 @@ import { describe, expect, it } from '@jest/globals';
 import { logger as console } from '../../../logger.js';
 
 import { twoStrings } from './two_strings.js';
-
-const TEST_CASES = [
-  {
-    title: 'Example 1 and 2',
-    comparing: [
-      {
-        s1: 'and',
-        s2: 'art',
-        expected: 'YES'
-      },
-      {
-        s1: 'be',
-        s2: 'cat',
-        expected: 'NO'
-      }
-    ]
-  },
-  {
-    title: 'Sample Test Case 0',
-    comparing: [
-      {
-        s1: 'hello',
-        s2: 'world',
-        expected: 'YES'
-      },
-      {
-        s1: 'hi',
-        s2: 'world',
-        expected: 'NO'
-      }
-    ]
-  },
-  {
-    title: 'Sample Test Case 6',
-    comparing: [
-      {
-        s1: 'wouldyoulikefries',
-        s2: 'abcabcabcabcabcabc',
-        expected: 'NO'
-      },
-      {
-        s1: 'hackerrankcommunity',
-        s2: 'cdecdecdecde',
-        expected: 'YES'
-      },
-      {
-        s1: 'jackandjill',
-        s2: 'wentupthehill',
-        expected: 'YES'
-      },
-      {
-        s1: 'writetoyourparents',
-        s2: 'fghmqzldbc',
-        expected: 'NO'
-      }
-    ]
-  }
-];
+import TEST_CASES from './two_strings.testcases.json';
 
 describe('two_strings', () => {
   it('twoStrings test cases', () => {
     expect.assertions(8);
 
     TEST_CASES.forEach((testCase) => {
-      testCase.comparing.forEach((test) => {
+      testCase.tests.forEach((test) => {
         const answer = twoStrings(test.s1, test.s2);
 
         console.debug(
