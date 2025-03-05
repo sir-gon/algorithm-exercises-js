5a404b315077d674fe0f3f63a94815ab3da2a9b5
Gonzalo Diaz <gonzalo@gon.cl>
2025-03-05 15:53:49 -0300

    [REFACTOR] [Hacker Rank] Interview Preparation Kit: Dictionaries and Hashmaps: Sherlock and Anagrams. * Adjusted the interface to match what hackerrank expects.

--- src/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/sherlock_and_anagrams.test.js
+++ src/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/sherlock_and_anagrams.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, it } from '@jest/globals';
 import { logger as console } from '../../../logger.js';
 
-import { sherlockAndAnagrams } from './sherlock_and_anagrams.js';
+import sherlockAndAnagrams from './sherlock_and_anagrams.js';
 import TEST_CASES from './sherlock_and_anagrams.testcases.json';
 
 describe('sherlock_and_anagrams', () => {
@@ -10,7 +10,7 @@ describe('sherlock_and_anagrams', () => {
 
     TEST_CASES.forEach((testSet) => {
       testSet.tests.forEach((test) => {
-        const answer = sherlockAndAnagrams(test.input);
+        const answer = sherlockAndAnagrams.sherlockAndAnagrams(test.input);
 
         console.debug(
           `sherlockAndAnagrams(${test.input}) solution found: ${answer}`
