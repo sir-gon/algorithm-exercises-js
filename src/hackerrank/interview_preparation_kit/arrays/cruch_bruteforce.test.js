3e4349f9c417390a3bafda55261be0e80362c4dd
Gonzalo Diaz <gonzalo@gon.cl>
2025-03-10 15:30:07 -0300

    [REFACTOR] [Hacker Rank] Interview Preparation Kit: Arrays: Array Manipulation. Adjusted the interface to match what hackerrank expects. Standarized way.

--- src/hackerrank/interview_preparation_kit/arrays/cruch_bruteforce.test.js
+++ src/hackerrank/interview_preparation_kit/arrays/cruch_bruteforce.test.js
@@ -3,14 +3,14 @@ import { logger as console } from '../../../logger.js';
 
 import TEST_CASES from './cruch_testcases_test.json';
 
-import crush from './cruch_bruteforce.js';
+import { arrayManipulation } from './cruch_bruteforce.js';
 
 describe('arrays: crush (bruteforce) small cases', () => {
   it('arrayManipulation Test Cases', () => {
     expect.assertions(3);
 
     TEST_CASES.forEach((test) => {
-      const answer = crush.arrayManipulation(test.n, test.queries);
+      const answer = arrayManipulation(test.n, test.queries);
 
       console.debug(
         `arrayManipulation(${test.n}, ${test.queries}) solution found: ${answer}`
