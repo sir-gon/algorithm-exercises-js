38e062880498806405a33ffeee474f4472349476
Gonzalo Diaz <devel@gon.cl>
2026-06-11 18:46:31 -0400

    Many fixes to upgrade Eslint from 9.x to 10.x

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,21 @@
+import neostandard from 'neostandard';
 import jest from 'eslint-plugin-jest';
 import prettier from 'eslint-plugin-prettier';
-import globals from 'globals';
-import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
-  {
-    ignores: ['**/coverage', '**/dist', '**/node_modules', 'eslint.config.js']
-  },
+  ...neostandard({
+    ignores: ['**/coverage', '**/dist', '**/node_modules', 'eslint.config.js'],
+  }),
   {
     files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: {
-        ...globals.node,
-        ...jest.environments.globals.globals
-      }
-    },
     plugins: {
       jest,
       prettier
     },
     rules: {
-      ...js.configs.recommended.rules,
       ...jest.configs.recommended.rules,
+      ...prettierConfig.rules,
       'prettier/prettier': ['error'],
       'no-restricted-syntax': 0,
       'no-console': 'off',
